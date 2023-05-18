3f396420458b7824ee74afb221ab35971ba946ea
HH <heather.hayes918@gmail.com>
2023-05-17 20:57:30 -0500

    task10

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,22 @@
+// Create a function named updateUniqueItems that returns an updated
+// map for all items with initial quantity at 1.
+// It should accept a map as an argument. The map it accepts for argument
+// is similar to the map you create in the previous task.
+// For each entry of the map where the quantity is 1, update the quantity
+// to 100. If updating the quantity is not possible (argument is not a map)
+// the error Cannot process should be thrown.
+
+export default function updateUniqueItems(map) {
+  // Check if the map is an instance of Map
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  map.forEach((value, key) => {
+    // Check if the value is 1
+    if (value === 1) {
+      // Update the value to 100
+      map.set(key, 100);
+    }
+  });
+  return map;
+}
