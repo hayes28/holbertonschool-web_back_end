3f95ce735ea00b4af8d31f04e5b8a0b21a97092c
HH <heather.hayes918@gmail.com>
2023-09-05 12:52:16 -0500

    Task1 with test file

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+"""Basic annotations - concat task 1"""
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings and returns a concatenated string"""
+    return (str1 + str2)
