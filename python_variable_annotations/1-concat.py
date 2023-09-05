d94c31d259a455cc195acfb406bc35e2245f4962
HH <heather.hayes918@gmail.com>
2023-09-05 12:57:24 -0500

    fixed pycodestyle

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -2,6 +2,7 @@
 
 """Basic annotations - concat task 1"""
 
+
 def concat(str1: str, str2: str) -> str:
     """Concatenate two strings and returns a concatenated string"""
     return (str1 + str2)
