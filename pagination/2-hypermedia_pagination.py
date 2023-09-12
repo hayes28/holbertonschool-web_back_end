00d9553af90332b7234f2683f97f094bd9f5548e
HH <heather.hayes918@gmail.com>
2023-09-12 17:09:57 -0500

    Task2 & test file

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""Replicate code from the previous task.
+Implement a get_hyper method that takes the same arguments (and defaults) as
+get_page and returns a dictionary containing the following key-value pairs"""
+import csv
+import math
+from typing import List, Dict
+
+
+def index_range(page: int, page_size: int):
+    """returns a tuple containing a start index and an end index"""
+    return ((page - 1) * page_size, page * page_size)
+
+
+class Server:
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                self.__dataset = [row for row in reader][1:]
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Returns the appropriate page of the dataset"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start_idx, end_idx = index_range(page, page_size)
+        return self.dataset()[start_idx:end_idx]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """Returns a dictionary containing the following key-value pairs"""
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages
+        }
