a5955a157b7a6c8f7e15ce255f34bd4018c6ca9a
HH <heather.hayes918@gmail.com>
2023-09-16 19:54:17 -0500

    Forgot docstrings

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -23,10 +23,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """ Constructor """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """ Filter values in incoming log records """
         original_message = record.getMessage()
         redacted_message = filter_datum(
             self.fields,
