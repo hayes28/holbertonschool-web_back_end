6f4fdac15b60c0413ce8a660981db8d37a78348b
HH <heather.hayes918@gmail.com>
2023-09-20 23:35:40 -0500

    Ok noted need both os imports

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -8,6 +8,7 @@ from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+import os
 
 
 app = Flask(__name__)
