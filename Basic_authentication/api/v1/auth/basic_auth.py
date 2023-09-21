2065ac9efdc76a15e7cfbc2ee1baf07da3641005
HH <heather.hayes918@gmail.com>
2023-09-21 11:51:12 -0500

    Task8

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -19,3 +19,19 @@ class BasicAuth(Auth):
         if authorization_header.startswith('Basic '):
             return authorization_header[6:]
         return None
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """ that returns the decoded value of a Base64 string
+        base64_authorization_header: Base64 string to decode
+        Return: the decoded value or None if fails """
+        import base64
+        if base64_authorization_header is None or \
+                type(base64_authorization_header) is not str:
+            return None
+        try:
+            return base64.b64decode(
+                base64_authorization_header.encode('utf-8')).decode('utf-8')
+        except Exception:
+            return None
