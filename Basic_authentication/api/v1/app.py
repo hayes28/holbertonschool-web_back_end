70e01f560c7663832f68ad4acbd14f7d7fc220d3
HH <heather.hayes918@gmail.com>
2023-09-20 23:19:03 -0500

    I think I got it

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -39,20 +39,16 @@ def not_found(error) -> str:
 
 
 @app.before_request
-def before_request_func() -> str:
-    """ Before request handler
-    """
+def before_request_func():
     if auth is None:
         return
-    excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
+    if auth.require_auth(request.path, ['/api/v1/status/',
+                                        '/api/v1/unauthorized/',
+                                        '/api/v1/forbidden/']):
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 if __name__ == "__main__":
