aee1e70fea75372a98ce9c226e9522e5a39900f3
HH <heather.hayes918@gmail.com>
2023-09-20 21:47:39 -0500

    Task4

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -10,9 +10,13 @@ class Auth:
     """ Auth class. """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Returns False - path and excluded_paths will not be used. """
-        return False
+        """Returns False if the path is in excluded_paths, True otherwise."""
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
 
+        path = path if path[-1] == '/' else f'{path}/'
+
+        return path not in excluded_paths
     def authorization_header(self, request=None) -> str:
         """ Returns None - request will not be used. """
         return None
