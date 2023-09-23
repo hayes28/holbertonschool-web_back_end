e5b74a4a43f634edd8d5f95bff26c3e663ed3446
HH <heather.hayes918@gmail.com>
2023-09-23 00:15:54 -0500

    Task 5

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -59,10 +59,12 @@ def before_request_func() -> str:
         return
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
+    if auth and not any(request.path.startswith(excluded) for excluded in excluded_paths) \
+        and (auth.authorization_header(request) is None and auth.session_cookie(request) is None):
         abort(401)
     user = auth.current_user(request)
     if user is None:
