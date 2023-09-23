8feb04eb0c766efd52ea13909e15177b37108481
HH <heather.hayes918@gmail.com>
2023-09-22 22:03:01 -0500

    Took logging debugger out

--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -19,7 +19,6 @@ def view_all_users() -> str:
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def view_one_user(user_id: str = None) -> str:
-    logging.debug(f"Inside /api/v1/users with Current User: {request.current_user}")
     """ GET /api/v1/users/:id
     Path parameter:
       - User ID
