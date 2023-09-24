98acd40693867d0fbca3973054fd666993aa5453
HH <heather.hayes918@gmail.com>
2023-09-24 18:38:12 -0500

    task 6

---
+++ user_authentication_service/app.py
@@ -0,0 +1,17 @@
+#!/use/bin/env python3
+""" Module of Authentication """
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def welcome():
+    """ GET /
+    Return:
+      - JSON payload
+    """
+    return jsonify({"message": "Bienvenue"}), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
