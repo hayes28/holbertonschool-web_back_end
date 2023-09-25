c0b77c0d9b8219ed240d2267263523bbb43ec795
HH <heather.hayes918@gmail.com>
2023-09-25 18:08:08 -0500

    retry

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -55,20 +55,21 @@ def login():
     return response
 
 
-@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+@app.route("/sessions", methods=["DELETE"])
 def logout():
     """ DELETE /sessions
-    JSON body:
-      - session_id
-    Return:
-      - JSON payload
-    """
-    session_id = request.cookies.get('session_id')
-    user = AUTH.get_user_from_session_id(session_id)
-    if not user:
+      JSON body:
+        - session_id
+      Return:
+        - JSON payload
+      """
+    session_id = request.cookies.get("session_id")
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            AUTH.destroy_session(user.id)
+            return redirect("/")
         abort(403)
-    AUTH.destroy_session(user.id)
-    return redirect('/')
 
 
 if __name__ == "__main__":
