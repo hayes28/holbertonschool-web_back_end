b2944330ae8b85633bd676b44d3a7ddee34ff7dc
HH <heather.hayes918@gmail.com>
2023-09-25 15:59:10 -0500

    task 10

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -33,6 +33,16 @@ class Auth:
         except NoResultFound:
             return False
 
+    def create_session(self, email: str) -> str:
+        """ Method that creates a session ID for a user."""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
+
 
 def _hash_password(password: str) -> bytes:
     """ Method that takes in a password
