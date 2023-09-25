b6dabdc2542641ddab40c09e4979060691a9f52a
HH <heather.hayes918@gmail.com>
2023-09-25 17:17:00 -0500

    Task 12

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -5,6 +5,7 @@ from user import User
 from sqlalchemy.orm.exc import NoResultFound
 from db import DB
 from uuid import uuid4
+from typing import Type
 
 
 class Auth:
@@ -40,6 +41,16 @@ class Auth:
             session_id = _generate_uuid()
             self._db.update_user(user.id, session_id=session_id)
             return session_id
+        except NoResultFound:
+            return None
+
+    def get_user_from_session_id(self, session_id: str) -> Type[User]:
+        """ Method that returns a User corresponding to a session ID or None """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user.email
         except NoResultFound:
             return None
 
