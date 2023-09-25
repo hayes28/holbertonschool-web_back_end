f4de25aad6deb3b9872c42838e7bb947183d76a0
HH <heather.hayes918@gmail.com>
2023-09-25 15:33:33 -0500

    Task 8

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -22,6 +22,16 @@ class Auth:
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Method that checks if a user exists
+        and validates password."""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
+        except NoResultFound:
+            return False
+
 
 def _hash_password(password: str) -> bytes:
     """ Method that takes in a password
