61c5cd95a49b4c012ab4d576f932088ccecb4671
HH <heather.hayes918@gmail.com>
2023-09-25 22:49:52 -0500

    Task 17

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -90,6 +90,20 @@ def profile():
     else:
         abort(403)
 
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token():
+    """ POST /reset_password
+      JSON body:
+        - email
+      Return:
+        - JSON payload"""
+    email = request.form.get("email")
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token})
+    except ValueError:
+        abort(403)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
