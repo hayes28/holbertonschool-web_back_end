b40947f32f2a1b32a4051b93db33ce7aca4eb723
HH <heather.hayes918@gmail.com>
2023-09-25 22:22:10 -0500

    I finally got task 14, 15 now 16 I hope

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -66,6 +66,17 @@ class Auth:
         except NoResultFound:
             return None
 
+    def get_reset_password_token(self, email: str) -> str:
+        """ Method that returns a reset
+        password token if it exists."""
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound as e:
+            raise ValueError from e
+
 
 def _hash_password(password: str) -> bytes:
     """ Method that takes in a password
