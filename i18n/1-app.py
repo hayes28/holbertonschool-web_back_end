ec98d255384f2ec226409043c41f9dfe3b6215fb
HH <heather.hayes918@gmail.com>
2023-10-02 15:16:06 -0500

    Had to update flask task1

---
+++ i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" 1. Basic Babel setup
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """ Babel configuration """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """ GET /
+    Return:
+      - 1-index.html
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
