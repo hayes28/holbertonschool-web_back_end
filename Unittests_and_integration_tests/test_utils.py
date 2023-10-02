fd6110262caec132219bb971db13f0be094b365e
HH <heather.hayes918@gmail.com>
2023-10-01 20:01:14 -0500

    Task 0

---
+++ Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Unittests and Integration Tests project """
+
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ TestAccessNestedMap Class """
+
+    @parameterized.expand([
+        ({'a': 1}, ('a',), 1),
+        ({'a': {'b': 2}}, ('a',), {'b': 2}),
+        ({'a': {'b': 2}}, ('a', 'b'), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """ Test access nested map """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ('a',), KeyError),
+        ({'a': 1}, ('a', 'b'), KeyError)
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """ Test access nested map exception """
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)
