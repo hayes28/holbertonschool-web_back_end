62cf713f451df5cdd9727425b80b5d14fc32f322
HH <heather.hayes918@gmail.com>
2023-10-02 14:58:46 -0500

    Task 0

---
+++ i18n/0-app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" 0. Basic Flask app """
+from flask import Flask, render_template
+
+
+print("Flask imported successfully")
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """ GET /
+    Return:
+      - 0-index.html
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
