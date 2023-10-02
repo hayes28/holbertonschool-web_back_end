92a0cbdb2208f9cf7af41306de325827064d9459
HH <heather.hayes918@gmail.com>
2023-10-02 16:02:04 -0500

    Getting error w/out init_app

---
+++ i18n/2-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" 2. Get locale from request
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, get_locale
+
+
+app = Flask(__name__)
+
+
+class Config:
+    """ Babel configuration """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """ GET /
+    Return:
+      - 1-index.html
+    """
+    return render_template('1-index.html')
+
+babel.init_app(app, locale_selector=get_locale)
+def get_locale():
+    """ Determine best match for supported languages
+    Return:
+      - Best language
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run()
