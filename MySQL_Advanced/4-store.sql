9abbed6ac5b56f4601801085844b22f42e6f448b
HH <heather.hayes918@gmail.com>
2023-10-15 19:48:36 -0500

    Task4

---
+++ MySQL_Advanced/4-store.sql
@@ -0,0 +1,12 @@
+-- Task 4 Create a trigger that decreases the quantity of an item after adding a new order.
+DELIMITER //
+CREATE TRIGGER decrease_quantity
+AFTER INSERT
+ON orders FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - 1
+    WHERE name = NEW.item_name;
+END;
+//
+DELIMITER ;
