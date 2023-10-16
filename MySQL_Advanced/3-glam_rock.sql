5aa6a63f1f4e71cd14ce68be209f4016e2f205f6
HH <heather.hayes918@gmail.com>
2023-10-15 19:52:41 -0500

    Wrong output bc the year was current

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,6 @@
 -- Task 3 List all bands with Glam Rock as main style
 -- Ranked by longeivity
-SELECT band_name,
-        IFNULL(split - formed, YEAR(CURDATE()) - formed) AS lifespan
+SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
