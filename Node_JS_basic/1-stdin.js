4e559f275bcf89e174792d752f83f25a1fe989a8
hayes28 <heather.hayes918@gmail.com>
2023-10-22 21:58:57 -0500

    Spaces 2 not 4

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,22 +5,22 @@ const readline = require('readline');
 
 // Create a readline interface to read from stdin and write to stdout
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-    });
+  input: process.stdin,
+  output: process.stdout
+});
 
 console.log('Welcome to Holberton School, what is your name?');
 
 // Set up an event listener for the 'line' event on the readline interface
 // This event is emitted whenever the user enters a new line of text
 rl.on('line', (input) => {
-    console.log(`Your name is: ${input}`);
+  console.log(`Your name is: ${input}`);
 
-    rl.close();
+  rl.close();
 });
 
 // Set up an event listener for the 'close' event on the readline interface
 // This event is emitted when the readline interface is closed
 rl.on('close', () => {
-    console.log('This important software is now closing');
+  console.log('This important software is now closing');
 });
