af369c35be7f752c61e7b6700bf093c74948d9a6
hayes28 <heather.hayes918@gmail.com>
2023-10-29 17:15:37 -0500

    Task 6

---
+++ unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,15 @@
+// Test cases: 6
+
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', function () {
+    it('should return a resolved promise', function (done) {
+        getPaymentTokenFromAPI(true)
+            .then((data) => {
+                expect(data).to.deep.equal({ data: 'Successful response from the API' });
+                done();
+            })
+            .catch((error) => done(error));
+    });
+});
