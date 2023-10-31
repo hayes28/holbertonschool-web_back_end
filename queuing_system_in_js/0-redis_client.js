1ad5ff036d08e3c5065e02ae79f0ea8344f81459
hayes28 <heather.hayes918@gmail.com>
2023-10-31 00:59:43 -0500

    task 1

---
+++ queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,12 @@
+// 1. Node Redis Client
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('ready', () => {
+    console.log('Redis client connected to the server')
+});
+
+client.on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err}`)
+});
