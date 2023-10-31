5287bbd4c7907f09f6889115e0e1472c166c14ad
hayes28 <heather.hayes918@gmail.com>
2023-10-30 21:23:10 -0500

    Again

--- unittests_in_js/9-api/api.js
+++ unittests_in_js/9-api/api.js
@@ -2,15 +2,14 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the payment system');
+app.get('/', function (request, response) {
+    response.send('Welcome to the payment system');
 });
 
-app.listen(7865, () => {
+app.get('/cart/:id([0-9]+)', function (request, response) {
+    response.send(`Payment methods for cart ${request.params.id}`);
+});
+
+app.listen(7865, function () {
     console.log('API available on localhost port 7865');
-})
-
-app.get('/cart/:id(\\d+)', (req, res) => {
-    res.send(`Payment methods for cart ${req.params.id}`);
 });
-
